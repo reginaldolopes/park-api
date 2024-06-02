77b1deef9d84b36f92369d4cdb78ac372e599ad6
Reginaldo Lopes (MAC) <reginaldoflopes@gmail.com>
2024-06-02 08:43:26 -0300

    Add DTO criação de usuário e response, e UsuarioMapper

---
+++ src/main/java/br/net/silvalopes/park_api/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,22 @@
+package br.net.silvalopes.park_api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCreateDto {
+
+    @NotBlank
+    @Email(message = "Formato do e-mail está inválido. Verifique!", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+    @NotBlank
+    @Size(min = 6, max = 8)
+    private String password;
+
+}
